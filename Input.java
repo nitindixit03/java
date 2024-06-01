271a9ce2a06da98d83d16385d5051702e488a438
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-01 13:25:33 +0530

    simple input

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class Input{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Roll No: ");
+        int rollno = input.nextInt();
+        System.out.println("your roll is " + rollno);
+
+    }
+}
