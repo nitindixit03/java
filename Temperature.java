e70284a621d58b10c3837f49387a01d7f2aea34a
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-01 14:14:46 +0530

    temperature converting

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter temperatue in Celsius: ");
+        float tempC = in.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println("the fahrenheit temperature of " + tempC + " celsius is " + tempF +"F");
+    }
+}
