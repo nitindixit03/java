9d38855297896c3f58c910e05235ee79b7a418cf
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-03 23:39:54 +0530

    Reverse number

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter the number: ");
+        int num = in.nextInt();
+        int RevNum;
+        while(num>0){
+            RevNum = num%10;
+            System.out.print(RevNum);
+            num = num/10;
+        }
+        // System.out.println(RevNum);
+    }
+}
+ 
