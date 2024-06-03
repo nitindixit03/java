00a3d89b689c1a475c8ebfafb624279e6877331c
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-03 22:15:37 +0530

    largest

---
+++ Largest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter three number to check which one is largest: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        if(a>b && b>c){
+            System.out.println("The lagrest number is " + a);
+        }
+        else if(b>c && b>a){
+            System.out.println("The lagrest number is " + b);
+        }
+        else{
+            System.out.println("The lagrest number is " + c);
+        }
+    }
+}
