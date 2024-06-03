e13612aae3b3257139ab4e573ecc24d7a412410e
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-03 23:27:52 +0530

    count numbers

---
+++ Countnums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Countnums {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("enter the number: ");
+        int num = in.nextInt();
+
+        int count = 0;
+
+        while (num>0) {
+            int rem = num %10;
+            if(rem == 3){
+                count++;
+            }
+            num = num/10;
+        }
+        System.out.println(count);
+    }
+}
