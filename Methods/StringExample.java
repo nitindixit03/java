701c1de1e5d1ab1e545faa8ede46a9346f46d295
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-06 03:25:22 +0530

    string

---
+++ Methods/StringExample.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StringExample {
+    public static void main(String[] arg){
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String greeting = "good morning!!";
+        return greeting;
+    }
+}
