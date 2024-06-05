28801c6feb3c01a27bbd6cd36e563583c5960433
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-06 03:34:32 +0530

    parameters

---
+++ Methods/Pararmeters.java
@@ -0,0 +1,13 @@
+
+
+public class Pararmeters {
+    public static void main(String[] args){
+        int ans = sum(10,10);
+        System.out.println(ans);
+    }
+
+    static int sum(int a,int b){
+        int sum = a + b;
+        return sum;
+    }
+}
