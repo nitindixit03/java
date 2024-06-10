d2c50701e0b155a295898e84e2631906f72799b9
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-10 22:52:09 +0530

    Matrix input output

---
+++ Array/Array.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int rows = in.nextInt();
+
+        System.out.println("Enter the number of columns: ");
+        int columns = in.nextInt();
+
+        int[][] matrix= new int[rows][columns];
+
+        System.out.println("Enter the elements in an array: ");
+
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<columns; j++){
+                matrix[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println("Matrix: ");
+
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<columns; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
