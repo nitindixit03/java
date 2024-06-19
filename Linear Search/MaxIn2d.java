3ccc30f1059c1b7db054839c172f9659936abdb3
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-19 16:37:15 +0530

    maximum 2d

--- Linear Search/MaxIn2d.java
+++ Linear Search/MaxIn2d.java
@@ -26,4 +26,26 @@ public class MaxIn2d {
     }
 }
 
+// or 2nd method:
+// public class MaxIn2d {
+//     public static void main(String[] args) {
+//         int[][] arr = {
+//             {1, 2, 3},
+//             {4, 5, 6, 10},
+//             {7, 8, 9}
+//         };
+//         int ans = search(arr);
+//         System.out.println("Max element in the 2D array: " + ans);
+//     }
 
+//     static int search(int[][] arr) {
+//         int max = Integer.MIN_VALUE;
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 if(arr[i][j]>max){
+//                     max = arr[i][j];
+//                 }
+//             }
+//         }return max;
+//     }
+// }  
